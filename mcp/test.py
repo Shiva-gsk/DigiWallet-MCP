c751e474315d5657851e9bf282e2fe451f3ca678
Shiva <shivagulapala2005@gmail.com>
2025-05-07 23:06:59 +0530

    Exp on HF api

---
+++ mcp/test.py
@@ -0,0 +1,117 @@
+from gradio_client import Client, handle_file
+
+client = Client("huggingface-projects/gemma-3-12b-it")
+result = client.predict(
+		message={"text":'''Can you list out my last 5 transactions from the given json data my userid is user_2uLeRedzQAkDxWNzzpSz3phKXNb carefully check my expensions whether credited or debited and give me total credit or debit based on it this is my json data: [
+    {
+      "id": "cm8ai88zh0000951w48a9hjiq",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "completed",
+      "value": 11
+    },
+    {
+      "id": "cm8bsy667000195cke9xbwxya",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "progress",
+      "value": 12
+    },
+    {
+      "id": "cm8bsy6ya000395ckttatu9lf",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "progress",
+      "value": 12
+    },
+    {
+      "id": "cm8bsy7fg000595cky0ofvrrm",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "progress",
+      "value": 12
+    },
+    {
+      "id": "cm8esem6u000195ho6u40cqzv",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "progress",
+      "value": 23
+    },
+    {
+      "id": "cm8k8jk6b000095t4o8yebdwk",
+      "from": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "to": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "status": "completed",
+      "value": 2
+    },
+    {
+      "id": "cm8ms41p6000095z04s8zn7jz",
+      "from": "user_2uJBqA8JL7FNsbogvCDR24DyJLa",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 11
+    },
+    {
+      "id": "cm8o8rnui0002jl03ctgd8rgh",
+      "from": "user_2unjkpbwK2MJT5iZXFUQUeioHeJ",
+      "to": "user_2unkQGwgh0BVoDEG9u9NpjXBNFO",
+      "status": "completed",
+      "value": 100
+    },
+    {
+      "id": "cm8o92lht000095d8p56mvbet",
+      "from": "user_2unkQGwgh0BVoDEG9u9NpjXBNFO",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 10
+    },
+    {
+      "id": "cm8oln1it0000958oz99ma93b",
+      "from": "user_2unYAjFnwP71jCzmN9zZWS5SreZ",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 50
+    },
+    {
+      "id": "cm8oog3og0001958oj7yxz34a",
+      "from": "user_2unkQGwgh0BVoDEG9u9NpjXBNFO",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 10
+    },
+    {
+      "id": "cm8oqida8000095bs3zysfgao",
+      "from": "user_2unkQGwgh0BVoDEG9u9NpjXBNFO",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 123
+    },
+    {
+      "id": "cm8oqqjax000095bow7vh6g33",
+      "from": "user_2unjkpbwK2MJT5iZXFUQUeioHeJ",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 111
+    },
+    {
+      "id": "cm8oqqm0u000395bo0uab2vs9",
+      "from": "user_2unjkpbwK2MJT5iZXFUQUeioHeJ",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "failed",
+      "value": 111
+    },
+    {
+      "id": "cm8oqqxox000495boc0r6hksw",
+      "from": "user_2unjkpbwK2MJT5iZXFUQUeioHeJ",
+      "to": "user_2uLeRedzQAkDxWNzzpSz3phKXNb",
+      "status": "completed",
+      "value": 11
+    }
+  ]
+  '''},
+		param_2="You are a helpful assistant.",
+		param_3=700,
+		api_name="/chat",
+)
+print(result)
