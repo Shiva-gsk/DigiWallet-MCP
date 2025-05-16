37d56f38fc3f79ac516f7bd6c0d4bcbbc2cde88e
Shiva <shivagulapala2005@gmail.com>
2025-05-16 12:30:14 +0530

    Integrated Stripe

--- digiwallet/components/CheckoutPage.tsx
+++ digiwallet/components/CheckoutPage.tsx
@@ -47,7 +47,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `http://www.localhost:3000/payment-success?amount=${amount}`,
+        return_url: `${process.env.DEPLOYMENT_URL}/payment-success?amount=${amount}`,
       },
     });
 
@@ -62,14 +62,15 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
 
     setLoading(false);
   };
-
+  
+  
   if (!clientSecret || !stripe || !elements) {
     return (
       <div className="flex items-center justify-center">
         <div
           className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
           role="status"
-        >
+          >
           <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
             Loading...
           </span>
@@ -77,8 +78,10 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       </div>
     );
   }
-
-  return (
+  
+      console.log("Client Secret:", clientSecret);
+  return (<>
+  
     <form onSubmit={handleSubmit} className="bg-white p-2 rounded-md">
       {clientSecret && <PaymentElement />}
 
@@ -87,10 +90,11 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       <button
         disabled={!stripe || loading}
         className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
-      >
+        >
         {!loading ? `Pay $${amount}` : "Processing..."}
       </button>
     </form>
+        </>
   );
 };
 
